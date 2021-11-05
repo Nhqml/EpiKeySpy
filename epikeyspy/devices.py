834e9884353c900c9022767e23cc8d705a1a0d49
Kenji Gaillac <kenji.gaillac@epita.fr>
2021-11-05 15:45:20 +0100

    events: fix Type values and add helper methods

--- epikeyspy/devices.py
+++ epikeyspy/devices.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 import enum
 import re
 import struct
+from datetime import datetime, timedelta
 from enum import Enum, IntFlag
 from pathlib import Path
 from typing import Optional
@@ -27,26 +28,38 @@ class Event:
         Enum of event types as provided by Linux Kernel.
         See https://www.kernel.org/doc/html/latest/input/event-codes.html#event-types
         """
-        EV_SYN = 1 << 0x00  # Event separator
-        EV_KEY = 1 << 0x01  # Key-like devices (e.g. keyboard)
-        EV_REL = 1 << 0x02  # Relative axis value change
-        EV_ABS = 1 << 0x03  # Absolute axis value change
-        EV_MSC = 1 << 0x04  # Misc
-        EV_SW = 1 << 0x05  # Binary state input switch
-        EV_LED = 1 << 0x11  # LEDs control
-        EV_SND = 1 << 0x12  # Send sound
-        EV_REP = 1 << 0x14  # Autorepeating devices
-        EV_FF = 1 << 0x15  # Send force feedback
-        EV_PWR = 1 << 0x16  # Power button / switch input
-        EV_FF_STATUS = 1 << 0x17  # Receive force feedback status
+        EV_SYN = 0x00  # Event separator
+        EV_KEY = 0x01  # Key-like devices (e.g. keyboard)
+        EV_REL = 0x02  # Relative axis value change
+        EV_ABS = 0x03  # Absolute axis value change
+        EV_MSC = 0x04  # Misc
+        EV_SW = 0x05  # Binary state input switch
+        EV_LED = 0x11  # LEDs control
+        EV_SND = 0x12  # Send sound
+        EV_REP = 0x14  # Autorepeating devices
+        EV_FF = 0x15  # Send force feedback
+        EV_PWR = 0x16  # Power button / switch input
+        EV_FF_STATUS = 0x17  # Receive force feedback status
+
+        def __str__(self):
+            return f'{self.name}'
 
     FORMAT = 'QQHHI'  # 24 bytes long: 2 unsigned long long, 2 unsigned short, 1 unsigned int
     SIZE = struct.calcsize(FORMAT)
 
-    def __init__(self, type_, code, value) -> None:
-        self._type = type_
+    def __init__(self, datetime, type_, code, value) -> None:
+        self.datetime = datetime
+        self.type_ = type_
         self.code = code
         self.value = value
+
+    def __repr__(self) -> str:
+        return f"<{self.__class__.__name__} datetime={self.datetime}, type={self.type_}, code={self.code}, value={self.value}>"
+
+    @classmethod
+    def from_raw(cls, raw_event: list[str]) -> Event:
+        sec, usec, type_, code, value = struct.unpack(Event.FORMAT, raw_event)
+        return Event(datetime.fromtimestamp(sec) + timedelta(microseconds=usec), cls.Type(type_), code, value)
 
 
 class Device():
