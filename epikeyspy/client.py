cb3c461965f1b06fd64db9acfc84c7abe1cca9cc
Kenji Gaillac <kenji.gaillac@epita.fr>
2021-11-05 16:46:23 +0100

    events: move events to separate file

--- epikeyspy/client.py
+++ epikeyspy/client.py
@@ -3,4 +3,5 @@ from .devices import list_devices, list_supported_devices
 
 def client_loop(server: str):
     devices = list_supported_devices()
-    devices[0].capture_events()
+    for event in devices[0].capture_events():
+        print(event)
--- epikeyspy/devices.py
+++ epikeyspy/devices.py
@@ -2,64 +2,14 @@ from __future__ import annotations
 
 import enum
 import re
-import struct
-from datetime import datetime, timedelta
-from enum import Enum, IntFlag
+from enum import Enum
 from pathlib import Path
 from typing import Optional
 
+from .events import Event
+
 DEVICES_PATH = Path('/proc/bus/input/devices')
 HANDLERS_PATH = Path('/dev/input')
-
-
-class Event:
-    """Input event.
-
-    According to Linux kernel documentation, this is the following structure:
-        struct input_event {
-            struct timeval time;
-            unsigned short type;
-            unsigned short code;
-            unsigned int value;
-        };
-    """
-    class Type(IntFlag):
-        """
-        Enum of event types as provided by Linux Kernel.
-        See https://www.kernel.org/doc/html/latest/input/event-codes.html#event-types
-        """
-        EV_SYN = 0x00  # Event separator
-        EV_KEY = 0x01  # Key-like devices (e.g. keyboard)
-        EV_REL = 0x02  # Relative axis value change
-        EV_ABS = 0x03  # Absolute axis value change
-        EV_MSC = 0x04  # Misc
-        EV_SW = 0x05  # Binary state input switch
-        EV_LED = 0x11  # LEDs control
-        EV_SND = 0x12  # Send sound
-        EV_REP = 0x14  # Autorepeating devices
-        EV_FF = 0x15  # Send force feedback
-        EV_PWR = 0x16  # Power button / switch input
-        EV_FF_STATUS = 0x17  # Receive force feedback status
-
-        def __str__(self):
-            return f'{self.name}'
-
-    FORMAT = 'QQHHI'  # 24 bytes long: 2 unsigned long long, 2 unsigned short, 1 unsigned int
-    SIZE = struct.calcsize(FORMAT)
-
-    def __init__(self, datetime, type_, code, value) -> None:
-        self.datetime = datetime
-        self.type_ = type_
-        self.code = code
-        self.value = value
-
-    def __repr__(self) -> str:
-        return f"<{self.__class__.__name__} datetime={self.datetime}, type={self.type_}, code={self.code}, value={self.value}>"
-
-    @classmethod
-    def from_raw(cls, raw_event: list[str]) -> Event:
-        sec, usec, type_, code, value = struct.unpack(Event.FORMAT, raw_event)
-        return Event(datetime.fromtimestamp(sec) + timedelta(microseconds=usec), cls.Type(type_), code, value)
 
 
 class Device():
@@ -84,7 +34,7 @@ class Device():
         self.handlers = handlers
 
     def __repr__(self):
-        return f"<Device: name='{self.name}', type={self.type_}, handlers={self.handlers}>"
+        return f"<{self.__class__.__name__} name='{self.name}', type={self.type_}, handlers={self.handlers}>"
 
     NAME_RE = re.compile('N: Name="(.*)"')
     SUPPORTED_HANDLERS_RE = [re.compile(handler) for handler in ['event[0-9]+']]
@@ -97,9 +47,7 @@ class Device():
         if handler in self.handlers:
             with open(HANDLERS_PATH / handler, 'rb') as events:
                 while True:
-                    raw_event = events.read(Event.SIZE)
-                    _, _, type_, code, value = struct.unpack(Event.FORMAT, raw_event)
-                    print(type_, code, value)
+                    yield Event.from_raw(events.read(Event.SIZE))
         else:
             raise ValueError(f'Invalid hander {handler} for device {self}')
 
